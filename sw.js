095eeade0969b8190697c43281beb47bbaaef0e8
skbmaksim82-byte <skbmaksim82@gmail.com>
2026-05-04 17:22:52 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-zakazov-v55';
+const CACHE_NAME = 'portal-zakazov-v58';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
