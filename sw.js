69fbbc60cb45b6130c89399f274b490b293c4154
skbmaksim82-byte <skbmaksim82@gmail.com>
2026-05-22 11:54:52 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,33 @@
-const CACHE_NAME = 'portal-zakazov-v58';
-const ASSETS = ['/', '/index.html'];
+const CACHE_NAME = 'kari-catalog-v1';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
